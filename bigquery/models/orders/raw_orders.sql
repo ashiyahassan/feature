296edf228611844c5b4f9015982331beadd3258c
ashiyahassan <ashiyahassan96@gamil.com>
2025-09-26 12:25:28 +0000

    feat: add new workflow definition

--- bigquery/models/orders/raw_orders.sql
+++ bigquery/models/orders/raw_orders.sql
@@ -10,15 +10,9 @@ WITH prepared AS (
     SELECT
         order_id,
         DATE(order_date) AS order_date,
-        customer_name,
-        order_amount,
-        order_status,
-        ROUND(order_amount * 0.1, 2) AS tax_amount, -- 10% of order_amount for tax
-        CASE
-            WHEN order_status = "Shipped" THEN "Completed"
-            ELSE "Pending"
-        END AS final_status -- Completed for shipped orders and Pending
-    FROM
+        order_name,
+       
+       FROM
         {{ source('orders_tbl', 'raw_orders') }}
 
     ORDER BY
